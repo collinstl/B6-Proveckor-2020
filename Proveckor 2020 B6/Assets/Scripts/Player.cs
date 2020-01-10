11cbc958c3ea4a64983e14431ec8716021d32fe9
D01k3n <55179191+D01k3n@users.noreply.github.com>
2020-01-10 10:01:53 +0100

    Player Movement - Finished

---
+++ Proveckor 2020 B6/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    PlayerController pc;
+
+    private void Start()
+    {
+        pc = FindObjectOfType<PlayerController>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (this.name == "Player1")
+        {
+            pc.jump1 = false; 
+        }
+        else
+        {
+            pc.jump2 = false; 
+        }
+    }
+}
