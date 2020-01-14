6d10358af3607fa24aa781332a42ae0545c1b09d
D01k3n <55179191+D01k3n@users.noreply.github.com>
2020-01-14 10:08:24 +0100

    Gameover Mechanic

--- Proveckor 2020 B6/Assets/Scripts/GameManager.cs
+++ Proveckor 2020 B6/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -15,12 +16,17 @@ public class GameManager : MonoBehaviour
     #endregion Camera - Variables, Alexander Dolk
     #region Random Events, Alexander Dolk   
     public float eventTimer = 10;
-    public GameObject catPrefab; 
-    public float catForce = 1000; 
+    public GameObject catPrefab;
+    public float catForce = 1000;
     #endregion Random Events, Alexander Dolk
+    public float countdownTimer = 5;
+    public bool isPaused = true; 
 
-
-
+    private void Awake()
+    {
+        Time.timeScale = 0.000000000001f;
+        StartCoroutine(StartGame());
+    }
     private void Start()
     {
         StartCoroutine(RandomEvent());
@@ -28,17 +34,17 @@ public class GameManager : MonoBehaviour
         cam = Camera.main;
         cameraRigidbody = cam.GetComponent<Rigidbody2D>();
         player1 = GameObject.Find("Player1").GetComponent<Transform>();
-        player2 = GameObject.Find("Player2").GetComponent<Transform>();        
+        player2 = GameObject.Find("Player2").GetComponent<Transform>();
         #endregion Camera - Components, Alexander Dolk
     }
 
     private void Update()
-    {        
+    {
         #region Camera, Alexander Dolk
         float cDistance1 = Vector2.Distance(new Vector2(player1.position.x, 0), new Vector2(cam.transform.position.x, 0));
         float cDistance2 = Vector2.Distance(new Vector2(player2.position.x, 0), new Vector2(cam.transform.position.x, 0));
-        if(cDistance1 + cDistance2 >= 11.3f) { cam.orthographicSize = (cDistance1 + cDistance2) * .44f; }
-        else if(cam.orthographicSize <= 7){ cam.orthographicSize = camDefaultSize; }
+        if (cDistance1 + cDistance2 >= 11.3f) { cam.orthographicSize = (cDistance1 + cDistance2) * .44f; }
+        else if (cam.orthographicSize <= 7) { cam.orthographicSize = camDefaultSize; }
         #endregion Camera, Alexander Dolk 
     }
 
@@ -47,7 +53,7 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(eventTimer);
         print("Random Event!");
         CatEvent();
-        StartCoroutine(RandomEvent()); 
+        StartCoroutine(RandomEvent());
     }
 
     void CatEvent()
@@ -55,11 +61,24 @@ public class GameManager : MonoBehaviour
         //Instantiate "cat" - Random position
         //add force - Across map
         int rand = Random.Range(1, 3); //Decides whether cat comes from left / right
-        GameObject cat;        
-        Vector2 pos; pos.x = (rand == 1) ? 42 + Random.Range(0, 10): -42 - Random.Range(0, 10); pos.y = -12; //Sets the original position of the newly spawned cat
+        GameObject cat;
+        Vector2 pos; pos.x = (rand == 1) ? 42 + Random.Range(0, 10) : -42 - Random.Range(0, 10); pos.y = -12; //Sets the spawn position of the newly spawned cat
         cat = Instantiate(catPrefab, pos, Quaternion.identity);
         Rigidbody2D catRigidbody = cat.GetComponent<Rigidbody2D>();
         Vector2 catDirection; catDirection.x = (rand == 1) ? -1 : 1; catDirection.y = 1; //Sets the correct direction for the cat to fly in 
         catRigidbody.AddForce(catDirection * catForce * Time.deltaTime); //Adds force to the cat
+    }
+
+    IEnumerator StartGame()
+    {
+        print("Starting Soon!");
+        yield return new WaitForSeconds(countdownTimer * Time.timeScale);
+        Time.timeScale = 1;
+        isPaused = false; 
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Temporary gameover reset
     }
 }
