31f795fa1ae534ce92d60409698fbfb31e49cfe7
D01k3n <55179191+D01k3n@users.noreply.github.com>
2020-01-25 20:16:09 +0100

    UI Scaling

--- B6-Proveckor-2020-c45fa6ebc703cd5ebffb59ac93bfdd54fd0ebbfc/Proveckor 2020 B6/Assets/Scripts/UIManager.cs
+++ B6-Proveckor-2020-c45fa6ebc703cd5ebffb59ac93bfdd54fd0ebbfc/Proveckor 2020 B6/Assets/Scripts/UIManager.cs
@@ -11,7 +11,45 @@ public class UIManager : MonoBehaviour
     public GameObject setting;
     [HideInInspector] public bool pauseIsActive = false;
 
-   
+    Vector2 resolution;
+
+    [System.Serializable]
+    public class UIComponent
+    {
+        public RectTransform Component;
+        [HideInInspector] public Vector2 sizePercentage;
+        [HideInInspector] public Vector2 posPercentage; 
+        
+    }
+    public UIComponent[] UIComponents;
+
+    Scaler scaler; 
+    private void Start()
+    {
+        scaler = GetComponent<Scaler>(); 
+        resolution = new Vector2(Screen.width, Screen.height);
+        for (int i = 0; i < UIComponents.Length; i++)
+        {
+            UIComponent component = UIComponents[i];
+            Vector2 size = scaler.sizeList[i];
+            Vector2 pos = scaler.posList[i];
+            component.sizePercentage = size;
+            component.posPercentage = pos; 
+            UIScaling(component.Component, component.sizePercentage.x, component.sizePercentage.y, component.posPercentage.x, component.posPercentage.y);
+        }
+    }
+
+    private void Update()
+    {
+        if (resolution.x != Screen.width || resolution.y != Screen.height)
+        {
+            for (int i = 0; i < UIComponents.Length; i++)
+            {
+                UIComponent component = UIComponents[i];
+                UIScaling(component.Component, component.sizePercentage.x, component.sizePercentage.y, component.posPercentage.x, component.posPercentage.y);
+            }
+        }
+    }
     public void PlayButton()
     {
         SceneManager.LoadScene("MainScene");
@@ -47,5 +85,16 @@ public class UIManager : MonoBehaviour
         Time.timeScale = 1;
     }
 
-   
+
+    void UIScaling(RectTransform component, float sizePercentageX, float sizePercentageY, float posPercentageX, float posPercentageY)
+    {
+        GameObject parent = component.parent.gameObject;       
+        RectTransform parentTransform = parent.GetComponent<RectTransform>();
+        
+        //Size
+        component.sizeDelta = new Vector2(parentTransform.sizeDelta.x * sizePercentageX, parentTransform.sizeDelta.y * sizePercentageY);
+
+        //Positioning
+
+    }
 }
